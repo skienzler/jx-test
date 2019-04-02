5c3fbcac6f7267ebcf29ced0b6d2e96d3ff9dda7
SimonKienzler <SimonKienzler@users.noreply.github.com>
2019-04-02 17:38:34 +0200

    Add Brownbag

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example Brownbag"
 
 	from := ""
 	if r.URL != nil {
